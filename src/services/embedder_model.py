421d944963bfbc25addaeeb661fea7514ca04b5e
VTR(Konstantin Avrukevich) <avrukievich@gmail.com>
2025-05-04 16:18:38 +0300

    model name is now can be selected from .env

--- src/services/embedder_model.py
+++ src/services/embedder_model.py
@@ -4,8 +4,9 @@ from sentence_transformers import SentenceTransformer
 
 
 def load_model() -> SentenceTransformer:
-    device = os.getenv("EMBEDDER_DEVICE", None)
-    model = SentenceTransformer("BAAI/bge-m3", device=device)
+    device = os.getenv("EMBEDDER_DEVICE", default="cpu")
+    model_name = os.getenv("EMBEDDER_MODEL", default="BAAI/bge-m3")
+    model = SentenceTransformer(model_name, device=device)
 
     if model.device.type == "cuda":
         model.half()
